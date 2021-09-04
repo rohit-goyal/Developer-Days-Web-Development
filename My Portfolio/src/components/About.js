885e7db021c16a16681d33dfdbdf37a777853699
Rohit Goyal <66769571+rohit-goyal@users.noreply.github.com>
2021-09-04 21:29:38 +0530

    Update About.js

--- My Portfolio/src/components/About.js
+++ My Portfolio/src/components/About.js
@@ -6,8 +6,8 @@ const About = () => {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hello, I am Jaidev.
-            <br className="hidden lg:inline-block" />I love to build amazing apps.
+            Hello, I am Rohit Goyal.
+            <br className="hidden lg:inline-block" />Interest in Back-end Development and Cloud Security & Cloud Computing.
           </h1>
           <p className="mb-8 leading-relaxed">
             It's been really amazing finding new things and aquiring skills and combining them to make something beautiful.Hoping to contine it with the same energy and excitement.
