af2d443ebc96a311359bab903072123bb8fe5514
Rohit Goyal <66769571+rohit-goyal@users.noreply.github.com>
2021-09-11 14:59:19 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,8 @@
+module.exports= getDate;
+
+function getDate(){
+  var today = new Date();
+  var options = { weekday: 'long', day: 'numeric', month: 'long'};
+  var day = today.toLocaleDateString("en-US", options);
+  return day;
+}
